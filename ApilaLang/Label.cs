b0ba3accd223acc9ce1dfa6c68f623b7d9c1b0c9
apilatosba <aposdapulos098@outlook.com>
2023-11-24 19:11:29 +0300

    commandBuffer working. but there is a strange bug in programming languge(not in mine but in c#. if i tell this bug to the microsoft will they hire me?)

---
+++ ApilaLang/Label.cs
@@ -0,0 +1,16 @@
+﻿namespace ApilaLang {
+   internal class Label {
+      public string name;
+      public int tokenIndex;
+      /// <summary>
+      /// The index of the command, in the commandBuffer, that the label points to ie. the command that comes after the label.
+      /// </summary>
+      public int commandIndex;
+
+      public Label(string name, int tokenIndex, int commandIndex) {
+         this.name = name;
+         this.tokenIndex = tokenIndex;
+         this.commandIndex = commandIndex;
+      }
+   }
+}
--- ApilaLang/Program.cs
+++ ApilaLang/Program.cs
@@ -12,8 +12,9 @@ namespace ApilaLang {
       /// <summary>
       /// Label name, index pairs. The index is the index of the label in the apilaCode array.
       /// </summary>
-      static Dictionary<string, int> labels = new Dictionary<string, int>();
+      static HashSet<Label> labels = new HashSet<Label>();
       static List<Action> commandBuffer = new List<Action>();
+      static int indexOfCommandToExecute = 0;
 
       static void Main(string[] args) {
          // Command line arguments
@@ -67,94 +68,144 @@ namespace ApilaLang {
 
             switch (tokenType) {
                case TokenType.Unknown: {
+                  int ii = i; /* This shouldnt matter. It is crazy. I dont even know, it maybe a bug in programming language */
+                  
                   // TODO: Print line and column number
-                  Console.WriteLine($"Error: Unknown token \"{apilaCode[i]}\".");
+                  Console.WriteLine($"Error: Unknown token \"{apilaCode[ii]}\".");
                   return;
                }
                case TokenType.StackElement: {
-                  stack.Push(double.Parse(apilaCode[i]));
+                  //stack.Push(double.Parse(apilaCode[i]));
+                  int ii = i; /* This shouldnt matter. It is crazy. I dont even know, it maybe a bug in programming language */
+                  commandBuffer.Add(() => stack.Push(double.Parse(apilaCode[ii])));
                   break;
                }
                case TokenType.Operator: {
-                  OperatorType operatorType = GetOperatorType(apilaCode[i]);
+                  int ii = i; /* This shouldnt matter. It is crazy. I dont even know, it maybe a bug in programming language */
 
+                  OperatorType operatorType = GetOperatorType(apilaCode[ii]);
+
+                  // TODO: try catch block should be moved to execution of commands
                   try {
                      switch (operatorType) {
                         case OperatorType.Add: {
-                           stack.Push(stack.Pop() + stack.Pop());
+                           //stack.Push(stack.Pop() + stack.Pop());
+                           commandBuffer.Add(() => stack.Push(stack.Pop() + stack.Pop()));
                            break;
                         }
                         case OperatorType.Substract: {
-                           double first = stack.Pop();
-                           double second = stack.Pop();
-                           stack.Push(second - first);
+                           //double first = stack.Pop();
+                           //double second = stack.Pop();
+                           //stack.Push(second - first);
+                           commandBuffer.Add(() => {
+                              double first = stack.Pop();
+                              double second = stack.Pop();
+                              stack.Push(second - first);
+                           });
                            break;
                         }
                         case OperatorType.Multiply: {
-                           stack.Push(stack.Pop() * stack.Pop());
+                           //stack.Push(stack.Pop() * stack.Pop());
+                           commandBuffer.Add(() => stack.Push(stack.Pop() * stack.Pop()));
                            break;
                         }
                         case OperatorType.Divide: {
-                           double first = stack.Pop();
-                           double second = stack.Pop();
-                           stack.Push(second / first);
+                           //double first = stack.Pop();
+                           //double second = stack.Pop();
+                           //stack.Push(second / first);
+                           commandBuffer.Add(() => {
+                              double first = stack.Pop();
+                              double second = stack.Pop();
+                              stack.Push(second / first);
+                           });
                            break;
                         }
                         case OperatorType.Print: {
-                           Console.WriteLine(stack.Pop());
+                           //Console.WriteLine(stack.Pop());
+                           commandBuffer.Add(() => Console.WriteLine(stack.Pop()));
                            break;
                         }
                         case OperatorType.Switch: {
-                           int firstIndex = (int)stack.Pop();
-                           int secondIndex = (int)stack.Pop();
+                           //int firstIndex = (int)stack.Pop();
+                           //int secondIndex = (int)stack.Pop();
 
-                           double first;
-                           double second;
+                           //double first;
+                           //double second;
 
-                           try {
-                              first = stack[firstIndex];
-                              second = stack[secondIndex];
-                           } catch (ArgumentOutOfRangeException) {
-                              // TODO: Print line and column number
-                              Console.WriteLine($"Error: At least one of the specified indexes were out of range in \"switch\". The specified indexes: \"{firstIndex}\" and \"{secondIndex}\"");
-                              return;
-                           }
+                           //try {
+                           //   first = stack[firstIndex];
+                           //   second = stack[secondIndex];
+                           //} catch (ArgumentOutOfRangeException) {
+                           //   // TODO: Print line and column number
+                           //   Console.WriteLine($"Error: At least one of the specified indexes were out of range in \"switch\". The specified indexes: \"{firstIndex}\" and \"{secondIndex}\"");
+                           //   return;
+                           //}
 
-                           stack[firstIndex] = second;
-                           stack[secondIndex] = first;
+                           //stack[firstIndex] = second;
+                           //stack[secondIndex] = first;
+                           commandBuffer.Add(() => {
+                              int firstIndex = (int)stack.Pop();
+                              int secondIndex = (int)stack.Pop();
 
+                              double first;
+                              double second;
+
+                              try {
+                                 first = stack[firstIndex];
+                                 second = stack[secondIndex];
+                              } catch (ArgumentOutOfRangeException) {
+                                 // TODO: Print line and column number
+                                 Console.WriteLine($"Error: At least one of the specified indexes were out of range in \"switch\". The specified indexes: \"{firstIndex}\" and \"{secondIndex}\"");
+                                 return; // TODO return to exit. this is not the only place where this happens
+                              }
+
+                              stack[firstIndex] = second;
+                              stack[secondIndex] = first;
+                           });
                            break;
                         }
                         case OperatorType.Duplicate: {
-                           stack.Push(stack.Peek());
+                           //stack.Push(stack.Peek());
+                           commandBuffer.Add(() => stack.Push(stack.Peek()));
                            break;
                         }
                         case OperatorType.Goto: {
-                           throw new NotImplementedException();
                            //string label = apilaCode[i + 1];
                            //int labelIndex = Array.FindIndex(apilaCode, i + 1, s => s == label);
 
                            //if (labelIndex == -1) { }
+                           commandBuffer.Add(() => {
+                              string label = apilaCode[ii + 1];
+                              indexOfCommandToExecute = labels.Where(l => l.name == $"{label}:").First().commandIndex;
+                           });
                            break;
                         }
                      }
                   } catch (InvalidOperationException) {
                      // TODO: Print line and column number
-                     Console.WriteLine($"Error: Not enough elements in the stack to perform the operation \"{apilaCode[i]}\".");
+                     Console.WriteLine($"Error: Not enough elements in the stack to perform the operation \"{apilaCode[ii]}\".");
                      return;
                   }
 
                   break;
                }
                case TokenType.Label: {
-                  if (!labels.TryAdd(apilaCode[i], i)) {
-                     Console.WriteLine($"Error: You have two labels with the same name. The label: \"{apilaCode[i]}\"");
+                  int ii = i; /* This shouldnt matter. It is crazy. I dont even know, it maybe a bug in programming language */
+
+                  if (labels.Where(l => l.name == apilaCode[ii]).Count() > 0) {
+                     Console.WriteLine($"Error: You have two labels with the same name. The label: \"{apilaCode[ii]}\"");
                      return;
+                  } else {
+                     labels.Add(new Label(apilaCode[ii], ii, commandBuffer.Count));
                   }
 
                   break;
                }
             }
+         }
+
+         for (;indexOfCommandToExecute < commandBuffer.Count; indexOfCommandToExecute++) {
+            commandBuffer[indexOfCommandToExecute]();
          }
       }
 
